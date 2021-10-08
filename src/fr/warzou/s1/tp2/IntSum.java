15f62f840fd86bb6f581dcd060b7a3ba104225a7
WarzouMc <warzoumc@gmail.com>
2021-10-08 15:27:40 +0200

    Int sum with a for....

--- src/fr/warzou/s1/tp2/IntSum.java
+++ src/fr/warzou/s1/tp2/IntSum.java
@@ -8,6 +8,13 @@ public class IntSum {
         Scanner scanner = new Scanner(System.in);
         int input = Math.abs(scanner.nextInt());
         System.out.println((input * (input + 1) / 2));
+
+        //fuck, with a for
+
+        int sum = 0;
+        for (int i = 0; i < input; i++)
+            sum += (i + 1);
+        System.out.println(sum);
     }
 
 }
